b8a034ceded3d0d1b22b30f7a6870270311b18ea
Kate-bzz <73671083+Kate-bzz@users.noreply.github.com>
2021-03-07 13:46:08 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str === undefined || bracketsConfig === undefined) {
+    return false;
+  }
+
+  let left = true;
+  while (str.length != 0 && left) {
+    left = false;
+
+    for (let config of bracketsConfig) {
+      let bracket = config.join('');
+      if (str.includes(bracket)) {
+        str = str.replace(bracket, '');
+        left = true;
+      }
+    }
+  }
+
+  return str.length == 0;
 }
